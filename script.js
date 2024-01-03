857d9b1c11345bee457e5632ca28d20f02f7946d
Epeny14 <epenyblaise@yahoo.fr>
2024-01-03 19:03:17 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// $(document).ready(function () {
+//     $('.slick-carousel').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//     });
+// });
+
+$(document).ready(function () {
+    $('.slick-carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
